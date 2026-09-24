61684ac66e26287e2f14e683dd9b15a522a03fb2
Rahul Parihar <rahulaauji71@gmail.com>
2026-09-25 01:38:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 var isDate = function (input) {
   //   write your code here
+	if(input instanceof Date){
+		        return !Number.isNaN(input.getTime());
+
+	}
+	if (typeof input === 'string') {
+        const timestamp = Date.parse(input);
+        return !Number.isNaN(timestamp);
+    }
+
+    // 3. Reject numbers, arrays, objects, booleans, etc.
+    return false;
 };
 
 // Do not change the code below.
 const input = prompt("Enter Date.");
+
 alert(isDate(input));
